f50d4e31c243921e4bf537c8f0c15b84bae42776
Niklas Saari <niklas.saari@tutanota.com>
2024-04-25 22:31:43 +0300

    fix: allow spaces in current working directory on cert generation

--- wazuh/certs/dashboard_http/generate_certs.sh
+++ wazuh/certs/dashboard_http/generate_certs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd $DIR
+cd "$DIR"
 
 openssl req -x509 -batch -nodes -days 365 -newkey rsa:2048 -keyout key.pem -out cert.pem
--- wazuh/certs/indexer_cluster/generate_certs.sh
+++ wazuh/certs/indexer_cluster/generate_certs.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd $DIR
+cd "$DIR"
 
 echo "Root CA"
 
